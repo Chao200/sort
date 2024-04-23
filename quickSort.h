0b298b52fdb30ff41771f515ce397f90df22dbc4
chao <xiaochao200@126.com>
2024-04-23 11:43:44 +0800

    add quickSort header&cpp

---
+++ quickSort.h
@@ -0,0 +1,8 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+int quickSort(std::vector<int>& nums, int left, int right);
+
+void randomQuickSort(std::vector<int>& nums, int left, int right);
+
+#endif
---
+++ quick_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+#include "quickSort.h"
+
+using namespace std;
+
+int quickSort(vector<int>& nums, int left, int right)
+{
+    int base = random() % (right - left + 1) + left;
+    swap(nums[left], nums[base]);
+    int index = left;
+    for (int i = left+1; i <= right; ++i)
+    {
+        if (nums[i] < nums[left])
+        {
+            index++;
+            swap(nums[i], nums[index]);
+        }
+    }
+    swap(nums[index], nums[left]);
+    return index;
+}
+
+
+void randomQuickSort(vector<int>& nums, int left, int right)
+{
+    if (left < right)
+    {
+        int pos = quickSort(nums, left, right);
+        randomQuickSort(nums, left, pos - 1);
+        randomQuickSort(nums, pos + 1, right);
+    }
+}
