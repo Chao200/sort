92fdc96eeaed2796309c9c450407084fb408571f
chao <xiaochao200@126.com>
2024-04-23 11:44:10 +0800

    add test_randomQuickSort

--- main.cpp
+++ main.cpp
@@ -14,8 +14,26 @@ void test_mergeSort()
     print(res);
 }
 
+
+void test_randomQuickSort()
+{
+    srand((unsigned)time(NULL));
+    // 返回 15 个 1~100 的随机数
+    auto nums = getRandomNumber(15);
+    print(nums);
+    cout << "After randomQuickSort\n";
+    randomQuickSort(nums, 0, nums.size()-1);
+    print(nums);
+}
+
 int main()
 {
+    cout << "=================mergerSort==================\n";
     test_mergeSort();
+    cout << "=============================================\n\n";
+
+    cout << "=================randomQuickSort=============\n";
+    test_randomQuickSort();
+    cout << "=============================================\n\n";
     return 0;
 }
