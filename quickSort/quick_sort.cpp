c7aba41d33b1f60e2198cc5ea0217ab3ad22a3b3
chao <xiaochao200@126.com>
2024-04-23 12:33:12 +0800

    add comment

--- quickSort/quick_sort.cpp
+++ quickSort/quick_sort.cpp
@@ -8,8 +8,11 @@ using namespace std;
 
 int quickSort(vector<int>& nums, int left, int right)
 {
+    // 随机选取 base
     int base = random() % (right - left + 1) + left;
     swap(nums[left], nums[base]);
+
+    // index 指向的是小于等于 base 的最后一个元素
     int index = left;
     for (int i = left+1; i <= right; ++i)
     {
@@ -19,7 +22,7 @@ int quickSort(vector<int>& nums, int left, int right)
             swap(nums[i], nums[index]);
         }
     }
-    swap(nums[index], nums[left]);
+    swap(nums[index], nums[left]);  // 和 base 交换
     return index;
 }
 
@@ -29,6 +32,7 @@ void randomQuickSort(vector<int>& nums, int left, int right)
     if (left < right)
     {
         int pos = quickSort(nums, left, right);
+        // 递归左右子区间
         randomQuickSort(nums, left, pos - 1);
         randomQuickSort(nums, pos + 1, right);
     }
