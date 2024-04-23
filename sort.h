4e0b8af180f65e405c2bd5f942d15f96bf2aff83
chao <xiaochao200@126.com>
2024-04-23 14:14:01 +0800

    add insert_sort

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include "insertSort/insert_sort.h"     /* 插入排序 */
 #include "mergeSort/merge_sort.h"   /* 归并排序 */
 #include "quickSort/quick_sort.h"   /* 随机快速排序 */
 #include "heapSort/heap_sort.h"     /* 堆排序 */
